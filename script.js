3d45d07e9bf5732305a51cc4c735f2dc6c7bb0d4
edgar_m <marquez.duo@gmail.com>
2021-03-03 21:17:40 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    var magic8Ball = {};
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+    magic8Ball.askQuestion = function(question){//Method
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+        $("#8ball").effect("shake");
+        $("#answer").text( answer );
+        $("#answer").fadeIn(4000);
+    };
+
+        var onClick = function() {
+          setTimeout(
+          function() {
+             //show prompt
+             var question = prompt("Ask a yes or no question")
+             magic8Ball.askQuestion(question)
+         }, 500);
+     };
+
+     $("#questionButton").click( onClick );
+     $("#answer").hide();
+});
